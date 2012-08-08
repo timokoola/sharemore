c2e9e6187e6df49865e84c2294d68c8d7d4632c5
Timo Koola <timo@moarub.com>
2012-08-08 23:44:50 +0300

    Added Copyright claims to source files and GPL license file

--- ShareMore/src/com/moarub/sharemoar/ShareToKipptActivity.java
+++ ShareMore/src/com/moarub/sharemoar/ShareToKipptActivity.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Moarub Oy.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Moarub Oy - initial API and implementation
+ ******************************************************************************/
 package com.moarub.sharemoar;
 
 import android.app.Activity;
